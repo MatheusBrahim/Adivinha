67db5a44a06253288f760c8ac773a4b63e6a2207
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:51:43 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("O que é, o que é?"); 
+Thread.Sleep(2000);
+
+Console.WriteLine("Fica no meio do coração.");
+Thread.Sleep(5000);
+
+Console.WriteLine("A letra 'a'");
